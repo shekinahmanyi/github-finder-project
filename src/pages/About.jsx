0023aeac4b499d9948fabc5d27e62b1f5cf7cbfd
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-21 12:57:44 +0100

    About page component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
-    </div>
+    <>
+      <h1 className='text-4xl mb-2 text-center'>Github Finder Project</h1>
+      <p className='mb-2 text-2xl font-mono ml-3 text-center'>
+        Hi i am Called Shekinah Manyi and I took this course and at the end of the day came out with a React app to search GitHub profiles and see profile details. This
+        project is part of the
+        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          {' '}
+          React Front To Back
+        </a>{' '}
+        Udemy course by
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy </a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400 text-center'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400 text-center'>
+        Layout By ~ 
+        <a className='text-white' href='https://twitter.com/shekinahmanyi'>
+           Shekinah Manyi
+        </a>
+      </p>
+    </>
   )
 }
 
