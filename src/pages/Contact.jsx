e95e75d909edd9a918b7d98d81b637d86ce8c225
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-21 12:58:50 +0100

    contact page component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
 
 function Contact() {
   return (
     <div>
-      Contact Page
+    <p className='mb-2 text-2xl font-mono ml-3 text-center'>
+        You can reach me at: 
+        <FaLinkedin className='justify-center'/> 
+        <FaGithub/> 
+        <FaTwitter/>
+        </p>
     </div>
   )
 }
