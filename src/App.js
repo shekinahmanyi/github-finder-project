5578c392319e48623153aed4ed734714a617467b
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-21 12:55:12 +0100

    Brought in other componenet pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router,Route } from "react-router-dom";
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
@@ -6,13 +10,18 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
-      <Navbar/>
-      <main className="container mx-auto px-3 pb-12">Content</main>
-
-      <Footer />
-
-      </div> 
-       </Router>
+        <Navbar />
+       
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
