17664765b8a0a14bff4dec5afca8953b9e85c2ff
shekinahmanyi <shekinahmanyi@gmail.com>
2023-02-21 13:00:25 +0100

    not found component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>
-     Notfound  
+    <div className='hero'>
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+            <h1 className="text-8xl font-bold mb-8">Oops!</h1>
+            <p className='text-5xl mb-8'>404 -Page not Found</p>
+            <Link to ='/' className='btn btn-primary btn-lg'>
+                <FaHome className='mr-2'/>
+               Back to HoME 
+            </Link>
+        </div>
+      </div>
     </div>
   )
 }
