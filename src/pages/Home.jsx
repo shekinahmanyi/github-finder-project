79e55b5de61aa97d56bb1193f9c12f0b402a59e6
shekinahmanyi <shekinahmanyi@gmail.com>
2023-04-25 10:01:11 +0100

    imported the search component in the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import UserResults from "../components/users/UserResults";
 
 function Home() {
   return (
-    <div>
-    <h1 className="text-6xl">Welcome</h1>
-    </div>
-  )
+    <> 
+    {/* Search Component */}
+      <UserResults />
+    </>
+  );
 }
 
-export default Home
+export default Home;
