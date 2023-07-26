899258022935eacba1bbec29156e90a4739221e8
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-26 12:41:11 +0100

    Did some random corrections

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
     <>
-      <h1 className='text-4xl mb-2 text-center'>Github Finder Project</h1>
-      <p className='mb-2 text-2xl font-mono ml-3 text-center'>
-        Hi i am Called Shekinah Manyi and I took this course and at the end of the day came out with a React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
+      <h1 className="text-4xl text-center">Github Finder Project</h1>
+      <p className=" text-xl font-mono ml-3 text-center">
+        Hi i am Called Shekinah Manyi and I took this course and at the end of<br/>
+        the day came out with a React app to search GitHub profiles and see<br/>
+        profile details. This project is part of the
+        <a href="https://www.udemy.com/course/modern-react-front-to-back/">
+          {" "}
           React Front To Back
-        </a>{' '}
+        </a>{" "}
         Udemy course by
         <strong>
-          <a href='https://traversymedia.com'> Brad Traversy </a>
+          <a href="https://traversymedia.com"> Brad Traversy </a>
         </strong>
         .
       </p>
-      <p className='text-lg text-gray-400 text-center'>
-        Version <span className='text-white'>1.0.0</span>
+      <p className="text-lg text-gray-400 text-center">
+        Version <span className="text-white">1.0.0</span>
       </p>
-      <p className='text-lg text-gray-400 text-center'>
-        Layout By ~ 
-        <a className='text-white' href='https://twitter.com/shekinahmanyi'>
-           Shekinah Manyi
+      <p className="text-lg text-gray-400 text-center">
+        Layout By ~
+        <a className="text-white" href="https://twitter.com/shekinahmanyi">
+          Shekinah Manyi
         </a>
       </p>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { FaLinkedin } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
-import { FaTwitter } from 'react-icons/fa'
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 function Contact() {
   return (
     <div>
-    <p className='mb-2 text-2xl font-mono ml-3 text-center'>
-        You can reach me at: 
-        <FaLinkedin className=' flex justify-center'/> 
-        <FaGithub/> 
-        <FaTwitter/>
-        </p>
+      <p className="mb-2 text-2xl font-mono ml-3 text-center flex flex-col justify-center items-center ">
+        You can reach me at:
+      </p>
+      <div className="flex justify-center items-center space-x-4 mb-4 text-3xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaTwitter />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
